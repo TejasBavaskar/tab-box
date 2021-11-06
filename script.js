e8add6e316f5d553af143020bb651db580e192d6
Tejas Bavaskar <44202599+TejasBavaskar@users.noreply.github.com>
2021-11-06 12:23:32 +0530

    feat(save-resources): added save resources tab (#4)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const checkBoxContainer = document.querySelector('.checkbox-list-container');
+const activeTabsDiv = document.querySelector('.active-tabs');
+const savedTabsDiv = document.querySelector('.saved-tabs');
+const addButton = document.getElementById('add-btn');
 
 chrome.storage.sync.get("color", ({ color }) => {
   console.log('Inside chrome function')
@@ -13,14 +15,61 @@ async function initiate() {
       console.log('openTabList: ', openTabList);
 
       openTabList.forEach((tab, index) => {
-        checkBoxContainer.innerHTML += `<div class="checkbox">
-                                          <label>
-                                            <input type="checkbox" value="">
-                                            <span>${tab.title}</span>
-                                          </label>
-                                        </div>
-                                      `;
+        activeTabsDiv.innerHTML += `<div class="checkbox">
+                                      <input type="checkbox" value="" id="checkbox-${index}">
+                                      <span>
+                                        <a href="${tab.url}" target="_blank">
+                                          ${tab.title}
+                                        </a>
+                                      </span>
+                                    </div>
+                                    `;
       })
     }
   )
 }
+
+window.onload = function() {
+  for(let i=0; i<localStorage.length; i++) {
+    const tabTitle = localStorage.key(i);
+    const url = localStorage[tabTitle];
+
+    savedTabsDiv.innerHTML += `<div class="checkbox">
+                                    <input type="checkbox" value="">
+                                    <span>
+                                      <a href="${url}" target="_blank">
+                                        ${tabTitle}
+                                      </a>
+                                    </span>
+                                  </div>
+                                `;
+
+  }
+}
+
+addButton.addEventListener('click', () => {
+  let saveCount = 0;
+  for(let i=0; i<openTabList.length; i++) {
+    const checkbox = document.getElementById(`checkbox-${i}`);
+    if(checkbox.checked) {
+      console.log(`${i} checked`);
+
+      localStorage.setItem(openTabList[i].title, openTabList[i].url);
+
+      savedTabsDiv.innerHTML += `<div class="checkbox">
+                                    <input type="checkbox" value="">
+                                    <span>
+                                      <a href="${openTabList[i].url}" target="_blank">
+                                        ${openTabList[i].title}
+                                      </a>
+                                    </span>
+                                  </div>
+                                `;
+      saveCount++;
+    }
+  }
+
+  if(saveCount > 0) {
+    alert('Saved Successfully!!!');
+  }
+})
